8944477d9d074d758839b1baf64d94f642974f80
EftekharAlam2 <hinsouvlmare@gmail.com>
2023-08-25 17:26:39 +0600

    providers section added for authentication

---
+++ src/AuthProviders/Providers.jsx
@@ -0,0 +1,82 @@
+import {
+  GithubAuthProvider,
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
+import app from "../Firebase/firebase.init";
+
+export const Context = createContext(null);
+
+const auth = getAuth(app);
+const goggleProvider = new GoogleAuthProvider();
+const githubProvider = new GithubAuthProvider();
+
+const Providers = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const updateUser = (user, name, photo) => {
+    return updateProfile(user, {
+      displayName: name,
+      photoURL: photo,
+    });
+  };
+
+  const signIn = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  const logOut = () => {
+    return signOut(auth);
+  };
+
+  const googleSignIn = () => {
+    return signInWithPopup(auth, goggleProvider);
+  };
+
+  const githubSignIn = () => {
+    return signInWithPopup(auth, githubProvider);
+  };
+
+  const authInfo = {
+    createUser,
+    updateUser,
+    signIn,
+    googleSignIn,
+    githubSignIn,
+    user,
+    logOut,
+    loading,
+  };
+
+  return (
+    <div>
+      <Context.Provider value={authInfo}>{children}</Context.Provider>
+    </div>
+  );
+};
+
+export default Providers;
