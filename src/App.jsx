12c9be0d194f34faa7bf03a3d43f35184d13192b
EftekharAlam2 <hinsouvlmare@gmail.com>
2023-08-25 21:02:56 +0600

    banner section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./SharedPage/Navbar";
 import Footer from "./SharedPage/Footer";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    AOS.init();
+  }, []);
   return (
     <div>
       <Navbar></Navbar>
--- src/Pages/Home/Banner.jsx
+++ src/Pages/Home/Banner.jsx
@@ -1,15 +1,35 @@
+import { LazyLoadImage } from "react-lazy-load-image-component";
+
 const Banner = () => {
   return (
     <div>
       <div className="carousel rounded-box mt-6 md:mt-10 flex justify-center md:gap-5">
-        <div className="carousel-item w-1/3 h-1/3 md:w-1/5 md:h-1/5">
-          <img src="https://i.ibb.co/mBSGpX5/dress.jpg" alt="Cothing" />
+        <div
+          data-aos="fade-right"
+          data-aos-duration="3000"
+          className="carousel-item w-1/3 h-1/3 md:w-1/5 md:h-1/5"
+        >
+          <LazyLoadImage
+            src="https://i.ibb.co/mBSGpX5/dress.jpg"
+            alt="Cothing"
+          />
         </div>
-        <div className="carousel-item w-1/3 h-1/3 md:w-1/5 md:h-1/5">
-          <img src="https://i.ibb.co/nbhpM1H/gadegt.jpg" alt="Tech" />
+        <div
+          data-aos="fade-up"
+          data-aos-duration="3000"
+          className="carousel-item w-1/3 h-1/3 md:w-1/5 md:h-1/5"
+        >
+          <LazyLoadImage src="https://i.ibb.co/nbhpM1H/gadegt.jpg" alt="Tech" />
         </div>
-        <div className="carousel-item w-1/3 h-1/3 md:w-1/5 md:h-1/5">
-          <img src="https://i.ibb.co/mBSGpX5/dress.jpg" alt="Cothing" />
+        <div
+          data-aos="fade-left"
+          data-aos-duration="3000"
+          className="carousel-item w-1/3 h-1/3 md:w-1/5 md:h-1/5"
+        >
+          <LazyLoadImage
+            src="https://i.ibb.co/mBSGpX5/dress.jpg"
+            alt="Cothing"
+          />
         </div>
       </div>
     </div>
