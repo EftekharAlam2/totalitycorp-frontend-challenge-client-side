eb179ac269d521a88b48dcc1a4e70f8cca400fab
EftekharAlam2 <hinsouvlmare@gmail.com>
2023-08-27 18:23:45 +0600

    checkout table added

--- src/Pages/MyCart/Checkout.jsx
+++ src/Pages/MyCart/Checkout.jsx
@@ -1,7 +1,78 @@
+import { useContext, useEffect, useState } from "react";
+import { Context } from "../../AuthProviders/Providers";
+
 const Checkout = () => {
+  const { user } = useContext(Context);
+
+  const [productData, setproductData] = useState([]);
+  const totalPrice = productData.reduce(
+    (total, product) => total + product.price,
+    0
+  );
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/product/${user?.email}`, {
+      method: "GET",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setproductData(data);
+      });
+  }, [user?.email]);
+
+  const handleDelete = (id) => {
+    fetch(`http://localhost:5000/product/${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.deletedCount > 0) {
+          alert("Deleted successful");
+          const remaining = productData.filter(
+            (products) => products._id !== id
+          );
+          setproductData(remaining);
+        }
+      });
+  };
+
   return (
-    <div className="mx-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 justify-center items-start">
-      <div className=" p-6 border rounded-lg shadow-md mb-5">
+    <div className="mx-5 grid grid-cols-1 md:grid-cols-1 lg:grid-cols-3 gap-10 justify-center mt-5 mb-5">
+      <div className="overflow-x-auto">
+        <h3 className="font-semibold text-2xl mb-4 ">
+          Total Cost: ${totalPrice.toFixed(2)}
+        </h3>
+        <table className="w-full bg-white border border-gray-300">
+          <thead>
+            <tr className="bg-blue-500 text-white">
+              <th className="py-2 px-4">Product Name</th>
+              <th className="py-2 px-4">Category</th>
+              <th className="py-2 px-4">Rating</th>
+              <th className="py-2 px-4">Price</th>
+              <th className="py-2 px-4"></th>
+            </tr>
+          </thead>
+          <tbody>
+            {productData.map((product) => (
+              <tr key={product._id} className="border-t">
+                <td className="py-2 px-4">{product.productName}</td>
+                <td className="py-2 px-4">{product.category}</td>
+                <td className="py-2 px-4">{product.rating}</td>
+                <td className="py-2 px-4">{product.price}</td>
+                <td className="py-2 px-4">
+                  <button
+                    onClick={() => handleDelete(product._id)}
+                    className="bg-red-500 text-white rounded-full py-2 px-4 hover:bg-red-700"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <div className=" p-6 border rounded-lg shadow-md ">
         <h2 className="text-xl font-semibold mb-4">Shipping Information</h2>
         <div className="mb-4">
           <label className="block text-sm font-medium mb-1" htmlFor="fullName">
@@ -97,7 +168,7 @@ const Checkout = () => {
           />
         </div>
       </div>
-      <div className="p-6 border rounded-lg shadow-md mt-5">
+      <div className="p-6 border rounded-lg shadow-md">
         <h2 className="text-xl font-semibold mb-4">Payment Details</h2>
         <div className="mb-4">
           <label
